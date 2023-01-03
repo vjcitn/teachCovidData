dd418ff3a44c3da8e8e33ea2935f1fe12600a423
vjcitn <stvjc@channing.harvard.edu>
2023-01-03 07:43:57 -0500

    dealing with snapshots and doc

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' snapshot of CDC data on COVID vaccination rates
+#' @docType data
+#' @format data.frame
+"cdc_vax_2022.12.21"	
+
+#' snapshot of CDC county-level data on COVID incidence in Mississippi
+#' @docType data
+#' @format data.frame
+"MS_counties_2022.12.21"
